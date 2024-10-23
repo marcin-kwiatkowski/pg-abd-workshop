a785eac1d927e060c2525c4b12a696bee59717b6
Marcin Kwiatkowski <m.kwiatkowski@pg.edu.pl>
2024-10-23 22:47:15 +0200

    POST /users endpoint

--- nestjs/src/users/entities/user.entity.ts
+++ nestjs/src/users/entities/user.entity.ts
@@ -1,11 +1,9 @@
 export class UserEntity {
   readonly id: string;
   readonly login: string;
-  readonly age: number;
 
-  constructor(id: string, login: string, age: number) {
+  constructor(id: string, login: string) {
     this.id = id;
     this.login = login;
-    this.age = age;
   }
 }
--- nestjs/src/users/users.controller.ts
+++ nestjs/src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserEntity } from './entities/user.entity';
 import { UserDTO } from './dtos/user.dto';
+import { CreateUserDTO } from './dtos/create-user.dto';
 
 @Controller()
 export class UsersController {
@@ -11,5 +12,11 @@ export class UsersController {
   async getAll(): Promise<UserDTO[]> {
     const users: UserEntity[] = await this.usersService.getAll();
     return users.map<UserDTO>((entity) => UserDTO.fromEntity(entity));
+  }
+
+  @Post('/users')
+  async createUser(@Body() createUser: CreateUserDTO): Promise<UserDTO> {
+    const user = await this.usersService.createUser(createUser.login);
+    return UserDTO.fromEntity(user);
   }
 }
