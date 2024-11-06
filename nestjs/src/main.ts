7a069726adda459590c69cbed62de95d45bfd97d
Marcin Kwiatkowski <m.kwiatkowski@pg.edu.pl>
2024-11-06 23:16:44 +0100

    SwaggerModule

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('REST API')
+    .setDescription('NestJS REST API')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
