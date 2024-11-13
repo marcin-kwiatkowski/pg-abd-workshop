bef44432224c6aecefd62802b1c22581eea1fa1d
Marcin Kwiatkowski <m.kwiatkowski@pg.edu.pl>
2024-11-13 22:16:38 +0100

    Added AAA sections

--- nestjs/src/users/users.controller.spec.ts
+++ nestjs/src/users/users.controller.spec.ts
@@ -17,23 +17,31 @@ describe('UsersController', () => {
 
   describe('getAll', () => {
     it('should return empty response when service returns an empty list', async () => {
+      // Arrange
       const result: UserEntity[] = [];
       jest
         .spyOn(usersService, 'getAll')
         .mockImplementation(() => Promise.resolve(result));
 
+      // Act
       const response = await usersController.getAll();
+
+      // Assert
       expect(response).toEqual(result);
     });
 
     it('should return proper user id', async () => {
+      // Arrange
       const userId = 'Some-ID';
       const result: UserEntity[] = [new UserEntity(userId, '')];
       jest
         .spyOn(usersService, 'getAll')
         .mockImplementation(() => Promise.resolve(result));
 
+      // Act
       const response = await usersController.getAll();
+
+      // Assert
       expect(response).toHaveLength(1);
       expect(response[0].id).toEqual(userId);
     });
@@ -41,6 +49,7 @@ describe('UsersController', () => {
 
   describe('createUser', () => {
     it('should return id of a created user', async () => {
+      // Arrange
       const userId = 'ID';
       const user = new UserEntity(userId, '');
 
@@ -48,8 +57,11 @@ describe('UsersController', () => {
         .spyOn(usersService, 'createUser')
         .mockImplementation(() => Promise.resolve(user));
 
+      // Act
       const payload = new CreateUserDTO('');
       const createdUserDTO = await usersController.createUser(payload);
+
+      // Assert
       expect(createdUserDTO).toBeDefined();
       expect(createdUserDTO.id).toEqual(userId);
     });
