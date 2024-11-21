571337311e5fd54132649e8c3e6f69477f2df4ab
Marcin Kwiatkowski <m.kwiatkowski@pg.edu.pl>
2024-11-21 17:04:35 +0100

    CORS

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -5,6 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+  });
+
   const config = new DocumentBuilder()
     .setTitle('REST API')
     .setDescription('NestJS REST API')
